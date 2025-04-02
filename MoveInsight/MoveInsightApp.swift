e1e9065f69a1b85e502420a37967a509834dd863
charliebgu573 <165543009+tpearspatial@users.noreply.github.com>
2025-04-02 23:30:23 +1300

    Initial Commit

---
+++ MoveInsight/MoveInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveInsightApp.swift
+//  MoveInsight
+//
+//  Created by Charlie Gu on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoveInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
